02b7843731ee282e1b4d489a648f26572e884ce7
Str1llax <artur.eelt@gmail.com>
2025-11-09 13:45:39 +0200

    Added player with animations and movement

---
+++ Game/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,98 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    
+    [Header("Components")]
+    [SerializeField] private InputAction playerMovement;
+    [SerializeField] private InputAction playerJump;
+    [SerializeField] private LayerMask groundLayer;
+    
+    private Rigidbody2D _rigidBody;
+    private BoxCollider2D _collider;
+    private Animator _playerAnimator;
+    private bool _facingRight = true;
+    
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
+        _playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        EnableMovement();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void EnableMovement()
+    {
+        playerMovement.Enable();
+        playerJump.Enable();
+    }
+
+    private void Move()
+    {
+        var moveInput = playerMovement.ReadValue<Vector2>();
+        var jumpInput = playerJump.IsPressed();
+        
+        // Flipping player when changing direction
+        if (moveInput.x < 0 && _facingRight)
+        {
+            Flip();
+        } 
+        else if (moveInput.x > 0 && !_facingRight)
+        {
+            Flip();
+        }
+        
+        _playerAnimator.SetBool("walking", moveInput.x != 0);
+        _playerAnimator.SetBool("grounded", isGrounded());
+        _rigidBody.linearVelocity = new Vector2(moveInput.x * moveSpeed, _rigidBody.linearVelocity.y);
+        
+        if (jumpInput && isGrounded())
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rigidBody.linearVelocity = new Vector2(_rigidBody.linearVelocityX, jumpForce);
+        _playerAnimator.SetTrigger("jump");
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        
+        var locScale = transform.localScale;
+        locScale.x *= -1;
+        transform.localScale = locScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            
+        }
+    }
+
+    private bool isGrounded()
+    {
+        var hitGround = Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size, 0f, Vector2.down, 0.05f, groundLayer);
+        return hitGround.collider is not null;
+    }
+}
